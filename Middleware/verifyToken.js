b054fce28146fce27029f77b515305f8d762ea3b
zercominternalrepo <repo@zercomsystems.com>
2022-05-14 11:17:02 +0100

    changig of jwt from header to authorization

--- Middleware/verifyToken.js
+++ Middleware/verifyToken.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken");
 
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.token;
+    const authHeader = req.headers['authorization'];
     if (authHeader) {
-        const token = authHeader.split(" ")[1]
+        const token = authHeader && authHeader.split(' ')[1]
         jwt.verify(token, process.env.SECRETK, (err, user) => {
             if (err) res.status(403).json("Token is Not Valid!");
             req.user = user
